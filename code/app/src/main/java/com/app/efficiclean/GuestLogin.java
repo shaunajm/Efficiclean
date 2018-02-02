3713421e6ef1fd563a779425302574a2b46f0c77
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2018-02-02 14:06:04 +0000

    [EFIC-17] Add Firebase user authentication functionality

--- code/app/src/main/java/com/app/efficiclean/GuestLogin.java
+++ code/app/src/main/java/com/app/efficiclean/GuestLogin.java
@@ -1,76 +1,89 @@
 package com.app.efficiclean;
 
-import android.app.AlertDialog;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.content.Intent;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.*;
-import java.util.ArrayList;
+import com.google.firebase.auth.FirebaseUser;
 
 public class GuestLogin extends AppCompatActivity {
 
-    private AlertDialog alertDialog;
-    private FirebaseDatabase db = FirebaseDatabase.getInstance();
-    private FirebaseAuth mAuth = FirebaseAuth.getInstance();
-    private DatabaseReference mGuestRef = db.getReference().child("guests");
-    private ArrayList<Guest> guestList;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText forename;
+    private EditText surname;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guest_login);
-        this.alertDialog = new AlertDialog.Builder(GuestLogin.this).create();
+
+        forename = (EditText) findViewById(R.id.etForename);
+        surname = (EditText) findViewById(R.id.etSurname);
+
+        loginBtn = (Button) findViewById(R.id.btLogin);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                loginButtonClick();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            mAuth.signOut();
+        }
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth fbAuth) {
+                FirebaseUser user = fbAuth.getCurrentUser();
+                if (user != null) {
+                    Intent staffLoginPage = new Intent(GuestLogin.this, GuestHome.class);
+                    startActivity(staffLoginPage);
+                }
+            }
+        };
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        mGuestRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                guestList = new ArrayList<Guest>();
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    Guest guest = ds.getValue(Guest.class);
-                    guestList.add(guest);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
-    public void loginButtonClick(View view) {
-        EditText forename = (EditText) findViewById(R.id.etForename);
-        EditText surname = (EditText) findViewById(R.id.etSurname);
+    public void loginButtonClick() {
+        forename = (EditText) findViewById(R.id.etForename);
+        surname = (EditText) findViewById(R.id.etSurname);
         String fString = forename.getText().toString().trim();
         String sString = surname.getText().toString().trim();
         if (fString.equals("staff1")) {
             Intent staffPage = new Intent(getApplicationContext(), StaffLogin.class);
             startActivity(staffPage);
         } else {
-            guestLogin(fString, sString);
+            mAuth.signInWithEmailAndPassword(fString, sString)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (!task.isSuccessful()) {
+                                Toast.makeText(GuestLogin.this, "Authentication failed.",
+                                        Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
         }
     }
-
-    private void guestLogin(String fString, String sString) {
-        for (Guest guest : this.guestList) {
-            if (guest.getForename().equals(fString) && guest.getSurname().equals(sString)) {
-                Intent guestPage = new Intent(getApplicationContext(), GuestHome.class);
-                startActivity(guestPage);
-            }
-        }
-    }
-
-    private void errorMessage() {
-        this.alertDialog.setMessage("Could not find details. Please try again.");
-        this.alertDialog.show();
-    }
-
 }
--- code/app/src/main/java/com/app/efficiclean/StaffLogin.java
+++ code/app/src/main/java/com/app/efficiclean/StaffLogin.java
@@ -2,61 +2,81 @@ package com.app.efficiclean;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import com.google.firebase.database.*;
-
-import java.util.ArrayList;
+import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class StaffLogin extends AppCompatActivity {
 
-    private FirebaseDatabase db = FirebaseDatabase.getInstance();
-    private DatabaseReference mStaffRef = db.getReference().child("staff");
-    private ArrayList<Staff> staffList;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText username;
+    private EditText password;
+    private Button loginBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_login);
+
+        username = (EditText) findViewById(R.id.etUsername);
+        password = (EditText) findViewById(R.id.etPassword);
+
+        loginBtn = (Button) findViewById(R.id.btStaffLogin);
+
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                staffLoginButtonClick();
+            }
+        });
+
+        mAuth = FirebaseAuth.getInstance();
+
+        if (mAuth.getCurrentUser() != null) {
+            mAuth.signOut();
+        }
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth fbAuth) {
+                FirebaseUser user = fbAuth.getCurrentUser();
+                if (user != null) {
+                    Intent staffPage = new Intent(StaffLogin.this, StaffHome.class);
+                    startActivity(staffPage);
+                }
+            }
+        };
     }
 
     @Override
     protected void onStart() {
         super.onStart();
 
-        mStaffRef.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                staffList = new ArrayList<Staff>();
-                for (DataSnapshot ds : dataSnapshot.getChildren()) {
-                    Staff staff = ds.getValue(Staff.class);
-                    staffList.add(staff);
-                }
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
+        mAuth.addAuthStateListener(mAuthListener);
     }
 
-    public void staffLoginButtonClick(View view) {
-        EditText username = (EditText) findViewById(R.id.etUsername);
-        EditText password = (EditText) findViewById(R.id.etPassword);
+    private void staffLoginButtonClick() {
         String uString = username.getText().toString().trim();
         String pString = password.getText().toString().trim();
-        staffLogin(uString, pString);
+        mAuth.signInWithEmailAndPassword(uString, pString)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (!task.isSuccessful()) {
+                            Toast.makeText(StaffLogin.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
-
-    private void staffLogin(String uString, String pString) {
-        for (Staff staff : this.staffList) {
-            if (staff.getUsername().equals(uString) && staff.getPassword().equals(pString)) {
-                Intent staffPage = new Intent(getApplicationContext(), StaffHome.class);
-                startActivity(staffPage);
-            }
-        }
-    }
-
 }
