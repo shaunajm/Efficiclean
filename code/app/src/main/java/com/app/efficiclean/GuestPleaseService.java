52c054d0577c8612457bb4c8db40f6ec8c7d9b56
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2018-02-09 17:04:09 +0000

    [EFIC-31] Create activities for result of Guest room status

---
+++ code/app/src/main/java/com/app/efficiclean/GuestPleaseService.java
@@ -0,0 +1,13 @@
+package com.app.efficiclean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GuestPleaseService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_guest_please_service);
+    }
+}
