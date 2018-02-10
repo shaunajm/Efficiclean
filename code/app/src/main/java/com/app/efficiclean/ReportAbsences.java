c284491e4093a91e8081168743ad0a069efd439e
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2018-02-10 11:21:27 +0000

    [EFIC-35] Add functionaltiy to get to Report Absence page

---
+++ code/app/src/main/java/com/app/efficiclean/ReportAbsences.java
@@ -0,0 +1,13 @@
+package com.app.efficiclean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ReportAbsences extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supervisor_report_absence);
+    }
+}
--- code/app/src/main/java/com/app/efficiclean/SupervisorHome.java
+++ code/app/src/main/java/com/app/efficiclean/SupervisorHome.java
@@ -1,13 +1,31 @@
 package com.app.efficiclean;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class SupervisorHome extends AppCompatActivity {
+
+    private Button hazardApproval;
+    private Button cleansApproval;
+    private Button breakApproval;
+    private Button reportSevereMess;
+    private Button reportAbsences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supervisor_home);
+
+        reportAbsences = (Button) findViewById(R.id.btReportAbsence);
+        reportAbsences.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(SupervisorHome.this, ReportAbsences.class);
+                startActivity(i);
+            }
+        });
     }
 }
