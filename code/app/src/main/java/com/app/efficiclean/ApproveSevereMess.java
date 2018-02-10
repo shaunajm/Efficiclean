03fa766776c00ce53b4fd5b13bfdf9982bf59e57
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2018-02-10 12:01:54 +0000

    [EFIC-36] Add functionality to access supervisor operations pages

---
+++ code/app/src/main/java/com/app/efficiclean/ApproveSevereMess.java
@@ -0,0 +1,13 @@
+package com.app.efficiclean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ApproveSevereMess extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supervisor_severe_mess_approval);
+    }
+}
---
+++ code/app/src/main/java/com/app/efficiclean/HazardApproval.java
@@ -0,0 +1,13 @@
+package com.app.efficiclean;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class HazardApproval extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supervisor_hazard_approval);
+    }
+}
