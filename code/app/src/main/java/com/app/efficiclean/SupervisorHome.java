45d5a01b2eef4f3bf2cde34b547000c75cd79223
Shauna Moran <shauna.moran34@mail.dcu.ie>
2018-02-13 19:00:45 +0000

    [EFIC-56] Read from database for Todays Teams and Supervisor home

--- code/app/src/main/java/com/app/efficiclean/SupervisorHome.java
+++ code/app/src/main/java/com/app/efficiclean/SupervisorHome.java
@@ -5,18 +5,31 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.app.efficiclean.classes.Approval;
+import com.app.efficiclean.classes.Supervisor;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class SupervisorHome extends AppCompatActivity {
 
+    private String supervisorKey;
+    private Supervisor supervisor;
+    private DatabaseReference mSuperRef;
     private Button hazardApproval;
     private Button cleansApproval;
     private Button breakApproval;
     private Button approveSevereMess;
     private Button reportAbsences;
-    private String staffKey;
     private String hotelID;
     private Bundle extras;
     private FirebaseAuth mAuth;
@@ -31,7 +44,7 @@ public class SupervisorHome extends AppCompatActivity {
         extras = getIntent().getExtras();
         if (extras != null) {
             hotelID = extras.getString("hotelID");
-            staffKey = extras.getString("staffKey");
+            supervisorKey = extras.getString("staffKey");
         }
 
         hazardApproval = (Button) findViewById(R.id.btHazardApproval);
@@ -84,6 +97,21 @@ public class SupervisorHome extends AppCompatActivity {
             }
         });
 
+        mSuperRef = FirebaseDatabase.getInstance().getReference(hotelID).child("supervisor").child(supervisorKey);
+        mSuperRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                supervisor = dataSnapshot.getValue(Supervisor.class);
+                setRoomApprovals();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
         mAuth = FirebaseAuth.getInstance();
         if (mAuth.getCurrentUser() != null) {
             mAuth.signOut();
@@ -104,4 +132,36 @@ public class SupervisorHome extends AppCompatActivity {
         //Add authentication listener
         mAuth.addAuthStateListener(mAuthListener);
     }
+
+    public void setRoomApprovals(){
+        TableLayout table = (TableLayout) findViewById(R.id.tbToBeApproved);
+        TextView template = (TextView) findViewById(R.id.tvRow1);
+
+        table.removeViews(1, table.getChildCount() - 1);
+        for(Approval approval : supervisor.approvals.values()) {
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            TextView roomNumber = new TextView(this);
+            roomNumber.setText(approval.getJob().getRoomNumber());
+            roomNumber.setTextSize(template.getTextSize() / 2);
+            roomNumber.setWidth(template.getWidth());
+            roomNumber.setHeight(template.getHeight());
+            roomNumber.setPadding(
+                    template.getPaddingLeft(),
+                    template.getPaddingTop() - 5,
+                    template.getPaddingRight(),
+                    template.getPaddingBottom());
+            roomNumber.setGravity(template.getGravity());
+
+            tr.addView(roomNumber);
+            table.addView(tr);
+
+        }
+    }
+
 }
+
+
--- code/app/src/main/java/com/app/efficiclean/TodaysTeams.java
+++ code/app/src/main/java/com/app/efficiclean/TodaysTeams.java
@@ -3,11 +3,27 @@ package com.app.efficiclean;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.ViewGroup;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.app.efficiclean.classes.Approval;
+import com.app.efficiclean.classes.Supervisor;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class TodaysTeams extends AppCompatActivity {
 
+    private String supervisorKey;
+    private String hotelID;
+    private Bundle extras;
+    private Supervisor supervisor;
+    private DatabaseReference mSuperRef;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -15,6 +31,26 @@ public class TodaysTeams extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_staff_todays_teams);
+
+        extras = getIntent().getExtras();
+        if (extras != null) {
+            hotelID = extras.getString("hotelID");
+            supervisorKey = extras.getString("staffKey");
+        }
+
+        mSuperRef = FirebaseDatabase.getInstance().getReference(hotelID).child("supervisor").child(supervisorKey);
+        mSuperRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                supervisor = dataSnapshot.getValue(Supervisor.class);
+                setRoomApprovals();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
         if (mAuth.getCurrentUser() != null) {
@@ -36,4 +72,35 @@ public class TodaysTeams extends AppCompatActivity {
         //Add authentication listener
         mAuth.addAuthStateListener(mAuthListener);
     }
+
+    public void setRoomApprovals(){
+        TableLayout table = (TableLayout) findViewById(R.id.tbToBeApproved);
+        TextView template = (TextView) findViewById(R.id.tvRow1);
+
+        table.removeViews(1, table.getChildCount() - 1);
+        for(Approval approval : supervisor.approvals.values()) {
+            TableRow tr = new TableRow(this);
+            tr.setLayoutParams(new TableLayout.LayoutParams(
+                    ViewGroup.LayoutParams.MATCH_PARENT,
+                    ViewGroup.LayoutParams.WRAP_CONTENT));
+
+            TextView roomNumber = new TextView(this);
+            roomNumber.setText(approval.getJob().getRoomNumber());
+            roomNumber.setTextSize(template.getTextSize() / 2);
+            roomNumber.setWidth(template.getWidth());
+            roomNumber.setHeight(template.getHeight());
+            roomNumber.setPadding(
+                    template.getPaddingLeft(),
+                    template.getPaddingTop() - 5,
+                    template.getPaddingRight(),
+                    template.getPaddingBottom());
+            roomNumber.setGravity(template.getGravity());
+
+            tr.addView(roomNumber);
+            table.addView(tr);
+
+        }
+    }
+
 }
+
