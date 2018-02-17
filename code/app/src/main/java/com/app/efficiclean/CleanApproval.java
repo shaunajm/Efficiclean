6b4e9b5462baad9f86f544abe917340ad9a00677
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2018-02-17 12:32:26 +0000

    [EFIC-66] Set up Supervisor approve selected Approval

--- code/app/src/main/java/com/app/efficiclean/CleanApproval.java
+++ code/app/src/main/java/com/app/efficiclean/CleanApproval.java
@@ -1,11 +1,13 @@
 package com.app.efficiclean;
 
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
-import android.widget.TextView;
+import android.view.View;
+import android.widget.*;
+import com.app.efficiclean.classes.Supervisor;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
 
 public class CleanApproval extends AppCompatActivity {
 
@@ -13,8 +15,15 @@ public class CleanApproval extends AppCompatActivity {
     private String hotelID;
     private String roomNumber;
     private Bundle extras;
+    private Supervisor supervisor;
+    private DatabaseReference mSuperRef;
+    private DatabaseReference mRootRef;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private CheckBox approve;
+    private CheckBox disapprove;
+    private EditText comments;
+    private Button btApprove;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,8 +40,41 @@ public class CleanApproval extends AppCompatActivity {
         }
 
         TextView header = (TextView) findViewById(R.id.tvRoomNumber);
-
         header.setText("Room: " + roomNumber);
+
+        approve = (CheckBox) findViewById(R.id.cbApprove);
+        disapprove = (CheckBox) findViewById(R.id.cbDisapprove);
+        comments = (EditText) findViewById(R.id.etComments);
+
+        btApprove = (Button) findViewById(R.id.btCleansApprovalSubmit);
+        btApprove.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (approve.isChecked()) {
+                    approvedSubmit();
+                } else if (disapprove.isChecked()) {
+                    disapprovedSubmit();
+                } else {
+                    Toast.makeText(CleanApproval.this, "You haven't selected an option. Please check one of the boxes and try again.",
+                            Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        mRootRef = FirebaseDatabase.getInstance().getReference(hotelID);
+
+        mSuperRef = FirebaseDatabase.getInstance().getReference(hotelID).child("supervisor").child(supervisorKey);
+        mSuperRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                supervisor = dataSnapshot.getValue(Supervisor.class);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -56,5 +98,13 @@ public class CleanApproval extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void approvedSubmit() {
+        
+    }
+
+    public void disapprovedSubmit() {
+
     }
 }
