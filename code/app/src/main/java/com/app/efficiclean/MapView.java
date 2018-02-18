b41f3918cf0f94b87e3e20429a0dd4179ab20c0b
Conor Hanlon <conor.hanlon5@mail.dcu.ie>
2018-02-18 12:54:32 +0000

    [EFIC-67] Add colour changing functionality to Map View

--- code/app/src/main/java/com/app/efficiclean/MapView.java
+++ code/app/src/main/java/com/app/efficiclean/MapView.java
@@ -1,13 +1,24 @@
 package com.app.efficiclean;
 
+import android.graphics.Color;
+import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-
+import android.util.Log;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.*;
+import com.sdsmdg.harjot.vectormaster.VectorMasterView;
+import com.sdsmdg.harjot.vectormaster.models.PathModel;
+
+import java.util.HashMap;
 
 public class MapView extends AppCompatActivity {
 
+    private HashMap<String, String> rooms;
+    private DatabaseReference mRoomRef;
+    private String hotelID;
+    private Bundle extras;
+    private VectorMasterView map;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
 
@@ -17,6 +28,34 @@ public class MapView extends AppCompatActivity {
         setContentView(R.layout.activity_map_view);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        extras = getIntent().getExtras();
+        if (extras != null) {
+            hotelID = extras.getString("hotelID");
+        }
+
+        map = (VectorMasterView) findViewById(R.id.vmMap);
+
+        mRoomRef = FirebaseDatabase.getInstance().getReference(hotelID).child("rooms");
+        mRoomRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                rooms = new HashMap<String, String>();
+                for (DataSnapshot ds : dataSnapshot.getChildren()) {
+                    String roomNumber = ds.getKey();
+                    String status = ds.child("status").getValue(String.class);
+                    if (roomNumber != null && status != null) {
+                        rooms.put(roomNumber, status);
+                    }
+                }
+                changeRoomColours();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -40,5 +79,29 @@ public class MapView extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void changeRoomColours() {
+        for (String roomNumber : rooms.keySet()) {
+            String status = rooms.get(roomNumber);
+            String pathName = "pRoom" + roomNumber;
+            Log.v("EFIC-RoomID", pathName);
+            Log.v("EFIC-Status", status);
+            PathModel room = map.getPathModelByName(pathName);
+            if (status.equals("Waiting")) {
+                room.setFillColor(Color.parseColor("#ffff00"));
+            } else if (status.equals("Do Not Disturb")) {
+                room.setFillColor(Color.parseColor("#000000"));
+            } else if (status.equals("To Be Cleaned")) {
+                room.setFillColor(Color.parseColor("#ff0000"));
+            } else if (status.equals("Completed")) {
+                room.setFillColor(Color.parseColor("#008000"));
+            } else if (status.equals("In Process")) {
+                room.setFillColor(Color.parseColor("#ff6600"));
+            } else {
+                room.setFillColor(Color.parseColor("#ffeeaa"));
+            }
+        }
+        map.update();
     }
 }
