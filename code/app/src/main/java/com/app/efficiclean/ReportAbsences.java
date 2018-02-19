ec11d3078d1d9adf791cabd48d258cff412b6185
Shauna Moran <shauna.moran34@mail.dcu.ie>
2018-02-19 10:20:54 +0000

    [EFIC-71] Populate report absence table

--- code/app/src/main/java/com/app/efficiclean/ReportAbsences.java
+++ code/app/src/main/java/com/app/efficiclean/ReportAbsences.java
@@ -3,13 +3,27 @@ package com.app.efficiclean;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
+import com.app.efficiclean.classes.Housekeeper;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class ReportAbsences extends AppCompatActivity {
 
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private String hotelID;
+    private Bundle extras;
+    private String supervisorKey;
+    private DatabaseReference mStaffRef;
+    private DataSnapshot staff;
+    private RadioGroup names;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +31,28 @@ public class ReportAbsences extends AppCompatActivity {
         setContentView(R.layout.activity_supervisor_report_absence);
         android.support.v7.app.ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
+
+        extras = getIntent().getExtras();
+        if (extras != null) {
+            hotelID = extras.getString("hotelID");
+            supervisorKey = extras.getString("staffKey");
+        }
+
+        names = (RadioGroup) findViewById(R.id.rgStaffList);
+
+        mStaffRef = FirebaseDatabase.getInstance().getReference(hotelID).child("staff");
+        mStaffRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                staff = dataSnapshot;
+                populateGroup();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
 
         mAuth = FirebaseAuth.getInstance();
 
@@ -40,6 +76,19 @@ public class ReportAbsences extends AppCompatActivity {
     public boolean onSupportNavigateUp() {
         onBackPressed();
         return true;
+    }
+
+    public void populateGroup(){
+        names.removeAllViews();
+        for (DataSnapshot ds : staff.getChildren()) {
+            Housekeeper hs = ds.getValue(Housekeeper.class);
+            String username = hs.getUsername();
+
+            RadioButton r1 = new RadioButton(this);
+            r1.setText(username);
+            r1.setLayoutParams(names.getLayoutParams());
+            names.addView(r1);
+        }
     }
 }
 
