fabc924288a7e320e6826e49d5037039d86999c2
Shauna Moran <shauna.moran34@mail.dcu.ie>
2018-02-24 13:44:12 +0000

    [EFIC-105] Add functionality for staff to add rooms to queue

--- code/app/src/main/java/com/app/efficiclean/activities/StaffMarkRoom.java
+++ code/app/src/main/java/com/app/efficiclean/activities/StaffMarkRoom.java
@@ -4,9 +4,15 @@ import android.content.pm.ActivityInfo;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TableLayout;
 
 import com.app.efficiclean.R;
+import com.app.efficiclean.classes.Job;
 import com.app.efficiclean.classes.QueueHandler;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
@@ -15,6 +21,8 @@ import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
+import java.util.HashMap;
+
 public class StaffMarkRoom extends AppCompatActivity {
 
     private String staffKey;
@@ -22,10 +30,12 @@ public class StaffMarkRoom extends AppCompatActivity {
     private Bundle extras;
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+    private DatabaseReference mRootRef;
+    private DatabaseReference mRoomRef;
     private DatabaseReference mJobRef;
-    private DatabaseReference mStaffRef;
-    private DataSnapshot staff;
-    private DataSnapshot jobs;
+    private RadioGroup status;
+    private EditText roomNumber;
+    private Button markRoom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,36 +47,27 @@ public class StaffMarkRoom extends AppCompatActivity {
         if(getResources().getBoolean(R.bool.landscape_only)){
             setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         }
+
         extras = getIntent().getExtras();
         if (extras != null) {
             hotelID = extras.getString("hotelID");
             staffKey = extras.getString("staffKey");
         }
-        mStaffRef = FirebaseDatabase.getInstance().getReference(hotelID).child("staff");
-        mStaffRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
+        roomNumber = (EditText) findViewById(R.id.etRoomNumber);
+        status = (RadioGroup) findViewById(R.id.rgStatus);
+
+        markRoom = (Button) findViewById(R.id.btMarkRoomSubmit);
+        markRoom.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                staff = dataSnapshot;
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        });
-
-        mJobRef = FirebaseDatabase.getInstance().getReference(hotelID).child("jobs");
-        mJobRef.addListenerForSingleValueEvent(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                jobs = dataSnapshot;
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
+            public void onClick(View v) {
+                addJob();
             }
         });
+
+        mRootRef = FirebaseDatabase.getInstance().getReference(hotelID);
+        mRoomRef = mRootRef.child("rooms");
+        mJobRef = mRootRef.child("jobs");
 
         mAuth = FirebaseAuth.getInstance();
         if (mAuth.getCurrentUser() != null) {
@@ -79,8 +80,6 @@ public class StaffMarkRoom extends AppCompatActivity {
 
             }
         };
-
-
     }
 
     @Override
@@ -96,5 +95,21 @@ public class StaffMarkRoom extends AppCompatActivity {
         super.onStop();
 
         mAuth.removeAuthStateListener(mAuthListener);
+    }
+
+
+    public void addJob() {
+        RadioButton rb = (RadioButton) findViewById(status.getCheckedRadioButtonId());
+        String roomStatus = rb.getText().toString();
+        String rNumber = roomNumber.getText().toString();
+        if (roomStatus.equals("Do not disturb")) {
+            mRoomRef.child(rNumber).child("status").setValue("Do Not Disturb");
+        } else {
+            Job job = new Job();
+            job.setRoomNumber(rNumber);
+            job.setPriority(0);
+            mRoomRef.child(rNumber).child("status").setValue("To Be Cleaned");
+            mJobRef.push().setValue(job);
+        }
     }
 }
